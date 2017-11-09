15307e31d9f827de2755bbe23e2401330c892d78
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-09 22:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    return 'This one is one me!'
+    return 'This one is on me!'
   }
 
   if (feet > 2000) {
@@ -15,5 +15,3 @@ function scuberGreetingForFeet(feet) {
 
 
 
-
-scuberGreetingForFeet(199)
