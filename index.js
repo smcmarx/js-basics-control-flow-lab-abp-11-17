06a07dc87abd462f84458a0f90a7dd97341793a1
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-09 22:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function ternaryCheckCity (city) {
 
 function switchOnCharmFromTip(tip) {
   switch (tip) {
-    case 'generous'
+    case 'generous':
       return 'Thank you so much.';
-    case 'not as generous' 
+    case 'not as generous':
       return 'Thank you.'
     default:
       return 'Bye.'
