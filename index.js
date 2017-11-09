d8f8cecbcc544322bc0076e4a4c958a2f2cebb4d
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-09 22:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,9 @@
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return 'This one is on me!'
-  }
-
-  if (feet > 2000) {
+  } else if (feet > 2000) {
     return 'I will gladly take your thirty bucks.'
-  }
-
-  if (feet > 2500) {
+  } else if (feet > 2500) {
     return 'No can do.'
   }
 }
-
-
-
